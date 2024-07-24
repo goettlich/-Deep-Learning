33a2768cb49c296adce2cb8408b3ba66ad4b41d4
Jamil Goettlich <jamil.goettlich@hhi.fraunhofer.de>
2024-07-24 18:12:30 +0200

    ddpm training mostly successful

--- diffusion_models/simple_unet.py
+++ diffusion_models/simple_unet.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+# Modified from https://github.com/obravo7/satellite-segmentation-pytorch/blob/master/models/unet.py
 
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None):
@@ -82,9 +83,9 @@ class OutConv(nn.Module):
         return self.conv(x)
 
 
-class UNet(nn.Module):
+class SegmentationUNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
-        super(UNet, self).__init__()
+        super(SegmentationUNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
@@ -119,9 +120,9 @@ class UNet(nn.Module):
     
 
 
-class SimpleUNet(nn.Module):
+class SmallSegmentationUNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
-        super(SimpleUNet, self).__init__()
+        super(SmallSegmentationUNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
@@ -153,4 +154,156 @@ class SimpleUNet(nn.Module):
         out = self.out(x)
         return out
     
+
+
+
+class SmallUNet(nn.Module):
+    def __init__(self, n_channels, bilinear=True):
+        super(SmallUNet, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+
+        self.first_layer = DoubleConv(n_channels, 8)
+        self.down1 = Down(8, 16) # maxpool -> doubleconv
+        self.down2 = Down(16, 32) # maxpool -> doubleconv
+
+        factor = 2 if bilinear else 1
+        self.down3 = Down(32, 64 // factor)
+        
+        self.up1 = Up(64, 32 // factor, bilinear) # upsample -> doubleconv
+        self.up2 = Up(32, 16 // factor, bilinear)  # upsample -> doubleconv
+        self.up3 = Up(16,8, bilinear)
+
+        self.out = OutConv(8, self.n_channels)
+
+    def forward(self, x):
+
+        x1 = self.first_layer(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+
+        out = self.out(x)
+        return out
+
+
+
+class SmallDiffstepConditionedUNet(nn.Module):
+    
+    # model conditioned on noise in first layer
+    # using embedding is not elegant because 
+    # 1) we would need to pre-define the embedding size
+    # (e.g. nn.Linear(1,x.width*x.height)) --> so the model could only work for one size
+    # 2) a senseless learnable layer
+    # instead, we just broadcast t into the respective shape in the forward pass 
+    # (and we scale it to a range between [0,1] to keep things nice for the network)
+    
+    def __init__(self, n_channels, n_diffsteps, bilinear=True):
+        super(SmallDiffstepConditionedUNet, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+        self.n_diffsteps = n_diffsteps
+
+        self.first_layer = DoubleConv(n_channels + 1, 8)
+        self.down1 = Down(8, 16) # maxpool -> doubleconv
+        self.down2 = Down(16, 32) # maxpool -> doubleconv
+
+        factor = 2 if bilinear else 1
+        self.down3 = Down(32, 64 // factor)
+        
+        self.up1 = Up(64, 32 // factor, bilinear) # upsample -> doubleconv
+        self.up2 = Up(32, 16 // factor, bilinear)  # upsample -> doubleconv
+        self.up3 = Up(16,8, bilinear)
+
+        self.out = OutConv(8, self.n_channels)
+
+    def forward(self, x, t):
+        
+        condition = torch.ones_like(x)*t.view(t.shape[0],1,1,1)/self.n_diffsteps
+        x = torch.concat([x,condition], dim=1)
+
+        x1 = self.first_layer(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+
+        out = self.out(x)
+        return out
+    
+
+
+class SmallMultiDiffstepConditionedUNet(nn.Module):
+    
+    # model conditioned on noise in first layer
+    # using embedding is not elegant because 
+    # 1) we would need to pre-define the embedding size
+    # (e.g. nn.Linear(1,x.width*x.height)) --> so the model could only work for one size
+    # 2) a senseless learnable layer
+    # instead, we just broadcast t into the respective shape in the forward pass 
+    # (and we scale it to a range between [0,1] to keep things nice for the network)
+    
+    def __init__(self, n_channels, n_diffsteps, initial_layer=32, multiplications=[1,2,4,8], bilinear=True):
+        super(SmallMultiDiffstepConditionedUNet, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+        self.n_diffsteps = n_diffsteps
+        features = [initial_layer*m for m in multiplications] 
+        
+        self.first_layer = DoubleConv(n_channels + 1, features[0])
+        self.down1 = Down(features[0] + 1, features[1]) # maxpool -> doubleconv
+        self.down2 = Down(features[1] + 1, features[2]) # maxpool -> doubleconv
+
+        factor = 2 if bilinear else 1
+        self.down3 = Down(features[2] + 1, features[3] // factor)
+        
+        self.up1 = Up(features[3]+1, features[2] // factor, bilinear) # upsample -> doubleconv
+        self.up2 = Up(features[2]+1, features[1] // factor, bilinear)  # upsample -> doubleconv
+        self.up3 = Up(features[1]+1, features[0], bilinear)
+
+        self.out = OutConv(features[0], self.n_channels)
+
+    def forward(self, x, t):
+        
+        B,C,H,W = x.shape
+        t = (t/self.n_diffsteps).clone()
+        t0 = t.view(B,1,1,1).expand(B,1,H,W)
+        x = torch.cat([x,t0], dim=1)
+        x1 = self.first_layer(x)
+        
+        B,C,H,W = x1.shape
+        t1 = t.view(B,1,1,1).expand(B,1,H,W)
+        x1 = torch.cat([x1,t1], dim=1)
+        x2 = self.down1(x1)
+
+        B,C,H,W = x2.shape
+        t2 = t.view(B,1,1,1).expand(B,1,H,W)
+        x2 = torch.cat([x2,t2], dim=1)
+        x3 = self.down2(x2)
+
+        B,C,H,W = x3.shape
+        t3 = t.view(B,1,1,1).expand(B,1,H,W)
+        x3 = torch.cat([x3,t3], dim=1)
+        x4 = self.down3(x3)
+
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+
+        out = self.out(x)
+        return out
+    
+
+
+
+
+
+
 
